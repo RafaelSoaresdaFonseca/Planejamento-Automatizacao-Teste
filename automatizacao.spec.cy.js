d6edf4b2239162dc318d6aa7658b2e638969f193
RafaelSoaresdaFonseca <rafaelsf51@gmail.com>
2022-08-30 14:15:57 -0300

    Adicionado preenchimento cadastro completo

--- automatizacao.spec.cy.js
+++ automatizacao.spec.cy.js
@@ -3,4 +3,21 @@ describe('Validação de campos', () => {
     beforeEach(() => {
         cy.visit('https://buger-eats.vercel.app');
     })
+
+
+    it('PreenchimentoCadastroCompletoExceto CNH', () => {
+        cy.get('a').click();
+        cy.get('input[placeholder="Nome completo"]').type('Rafael Fonseca');
+        cy.get('input[placeholder="CPF somente números"]').type('12345678910');
+        cy.get('input[placeholder="E-mail"]').type('aaaa@gmail.com');
+        cy.get('input[placeholder="Whatsapp"]').type('32987654321');
+        cy.get(':input[placeholder="CEP"]').type('36010001');
+        cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2) > input').click();
+        cy.get('input[placeholder="Número"]').type('123456');
+        cy.get('input[placeholder="Complemento"]').type('ap10256');
+        cy.get('.delivery-method > :nth-child(2)').click();
+        cy.get('.button-success').click();
+        cy.contains('span.alert-error','Adicione uma foto da sua CNH').should('be.visible')      
+        
+    })
 })
